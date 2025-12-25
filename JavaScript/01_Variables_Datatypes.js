c80ff507d6adf0e3b777f9861db7f4d9e9944e00
Shreyash Sature <shreyashsature@gmail.com>
2025-12-25 14:44:29 +0530

    Java Script

---
+++ JavaScript/01_Variables_Datatypes.js
@@ -0,0 +1,26 @@
+console.log("Shreyash");
+console.log("I am a student, learning JS.")
+
+let fullName = "Shreyash";
+let age = 20;
+console.log(fullName,age);
+
+const student = {
+    fullName : "Shreyash",
+    age : 21,
+    cgpa : 8.07
+
+};
+console.log(student);
+
+const obj = {
+    type : "Pen",
+    name : "Parker Jotter Standard CT Ball Pen (Black)",
+    rating : 4,
+    price : 270,
+    discount : "5%"
+
+};
+console.log(obj.type);
+
+
