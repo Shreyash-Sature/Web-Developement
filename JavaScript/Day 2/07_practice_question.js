c52b9918bcbfcee3e36bcefb04d09ef5a8308a7e
Shreyash Sature <shreyashsature@gmail.com>
2026-04-01 16:08:23 +0530

    Practice question

---
+++ JavaScript/Day 2/07_practice_question.js
@@ -0,0 +1,9 @@
+// if string starts with letter 'a' and have length > 3 then its a good string
+
+let str = "adinath";
+if(str[0]=='a' && str.length >3){
+    console.log("This is a good string.");
+}
+else{
+    console.log("This is not a good string.");
+}
